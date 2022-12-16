5f5871fd008685c83ac32557f0f137a4cdd10a63
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2022-12-16 17:08:33 +0300

    create the signup page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./components/UI/Login";
-import HomePage from "./components/UI/HomePage";
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from './components/UI/Login'
+import HomePage from './components/UI/HomePage'
+import Signup from './components/UI/Signup'
 function App() {
-	return (
-		<>
-			<Router>
-				<>
-					<div className="App">
-						<Routes>
-							<Route exact path={"/"} element={<Login />} />
-							<Route exact path={"/home"} element={<HomePage />} />
-						</Routes>
-					</div>
-				</>
-			</Router>
-		</>
-	);
+  return (
+    <>
+      <Router>
+        <>
+          <div className='App'>
+            <Routes>
+              <Route exact path={'/'} element={<Login />} />
+              <Route exact path={'/home'} element={<HomePage />} />
+              <Route exact path={'/signup'} element={<Signup />} />
+            </Routes>
+          </div>
+        </>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
