0615888ce97c70a7d5efddfa4a76f894c73f7074
DenisOuma <dennisouma38@gmail.com>
2022-12-16 19:25:28 +0300

    side bar navbar additional and css with view more button

--- src/components/reusable/NavigationBar.jsx
+++ src/components/reusable/NavigationBar.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "../../styles/NavigationBar.css";
+import logo from "../../img/logo.png";
+import { Icon } from "@iconify/react";
+
+import IconButton from "@mui/material/IconButton";
 function NavigationBar() {
 	return (
 		<div className="dash-board-section">
 			<div className="side-bar">
-				<div className="top-logo-nav"></div>
+				<div className="top-logo-nav">
+					<div className="logo-section">
+						<img src={logo} alt="" />
+					</div>
+					<IconButton aria-label="delete">
+						<Icon icon="material-symbols:expand-more-rounded" />
+					</IconButton>
+				</div>
 			</div>
 		</div>
 	);
