9c0beaf722bb599d9dbb0ad694df10c902a9fea8
DenisOuma <dennisouma38@gmail.com>
2022-12-16 20:19:50 +0300

    Setting up the routes for diffrent page on the sidebar section

--- src/components/reusable/NavigationBar.jsx
+++ src/components/reusable/NavigationBar.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import "../../styles/NavigationBar.css";
 import logo from "../../img/logo.png";
 import { Icon } from "@iconify/react";
+import { Link } from "react-router-dom";
+import { IconButton } from "@mui/material";
 
-import IconButton from "@mui/material/IconButton";
 function NavigationBar() {
 	return (
 		<div className="dash-board-section">
@@ -15,6 +16,73 @@ function NavigationBar() {
 					<IconButton aria-label="delete">
 						<Icon icon="material-symbols:expand-more-rounded" />
 					</IconButton>
+				</div>
+				<div className="nav-menus">
+					<div className="nav-menu">
+						<Link to="/myportfolio/" className="icon-name">
+							<div className="options-menu">
+								<Icon
+									icon="ic:round-search"
+									color="#666363 "
+									height="20px"
+									width="20px"
+								/>
+								<p>Search</p>
+							</div>
+						</Link>
+					</div>
+					<div className="nav-menu">
+						<Link to="/myportfolio/" className="icon-name">
+							<div className="options-menu">
+								<Icon
+									icon="material-symbols:home-outline-rounded"
+									color="#666363 "
+									height="20px"
+									width="20px"
+								/>
+								<p>Home</p>
+							</div>
+						</Link>
+					</div>
+					<div className="nav-menu">
+						<Link to="/myportfolio/" className="icon-name">
+							<div className="options-menu">
+								<Icon
+									icon="ic:outline-notifications-none"
+									color="#666363 "
+									height="20px"
+									width="20px"
+								/>
+								<p>Notifications</p>
+							</div>
+						</Link>
+					</div>
+					<div className="nav-menu">
+						<Link to="/myportfolio/" className="icon-name">
+							<div className="options-menu">
+								<Icon
+									icon="tabler:message-circle"
+									color="#666363 "
+									height="20px"
+									width="20px"
+								/>
+								<p>Direct Messages</p>
+							</div>
+						</Link>
+					</div>
+					<div className="nav-menu">
+						<Link to="/myportfolio/" className="icon-name">
+							<div className="options-menu">
+								<Icon
+									icon="ri:group-line"
+									color="#666363 "
+									height="20px"
+									width="20px"
+								/>
+								<p>Memmbers</p>
+							</div>
+						</Link>
+					</div>
 				</div>
 			</div>
 		</div>
