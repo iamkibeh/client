f068bbd87f6c7ae2b874b6d8faf42fad078a7a48
DenisOuma <dennisouma38@gmail.com>
2022-12-18 10:33:02 +0300

    Adding navbar and create Post button

--- src/components/pages/PostFeeds.jsx
+++ src/components/pages/PostFeeds.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import Select from "react-select";
 import NavBarCard from "../reusable/NavBarCard";
-
+import Button from "@mui/material/Button";
+const options = [
+	{ value: "All ", label: "All" },
+	{ value: "Latest", label: "Latest" },
+	{ value: "Oldest", label: "Oldest" },
+];
 function PostFeeds() {
+	const [selectedOption, setSelectedOption] = useState("null");
 	return (
-		<div>
+		<div className="prime-container">
 			<NavBarCard className="cad-items">
 				<h1>Home</h1>
-				<div className="home-details"></div>
+				<div className="home-details">
+					<div className="category-post">
+						<Select
+							defaultValue={selectedOption}
+							onChange={setSelectedOption}
+							options={options}
+							placeholder="All Posts"
+						/>
+					</div>
+					<div className="add-post">
+						{" "}
+						<Button variant="contained" color="success">
+							Success
+						</Button>
+					</div>
+				</div>
 			</NavBarCard>
 		</div>
 	);
