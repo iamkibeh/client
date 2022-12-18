3bda06d7de672e0420eccd2289eb894750f376de
DenisOuma <dennisouma38@gmail.com>
2022-12-18 20:08:34 +0300

    Posts are able to be scrolled

--- src/components/pages/PostFeeds.jsx
+++ src/components/pages/PostFeeds.jsx
@@ -10,6 +10,36 @@ const options = [
 	{ value: "Oldest posts", label: "Oldest posts" },
 ];
 function PostFeeds() {
+	const posts = [
+		{
+			id: 1,
+			profileImage:
+				"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+			userName: "Denis Ouma",
+			createdAt: "2 hours ago",
+			profession: "Software Engineer",
+			postTitle: "How to Become a data analyst by spending 0$",
+			postComment:
+				"A lot of you want to know how much time is required to become Data Analyst. Here is a tool-by-tool learning roadmap for freshers",
+			postImg:
+				"https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1506&q=80",
+			comments: [1, 2, 3, 4, 5, 6],
+		},
+		{
+			id: 1,
+			profileImage:
+				"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+			userName: "Denis Ouma",
+			createdAt: "2 hours ago",
+			profession: "Software Engineer",
+			postTitle: "How to Become a data analyst by spending 0$",
+			postComment:
+				"A lot of you want to know how much time is required to become Data Analyst. Here is a tool-by-tool learning roadmap for freshers",
+			postImg:
+				"https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1506&q=80",
+			comments: [1, 2, 3, 4, 5, 6],
+		},
+	];
 	const [selectedOption, setSelectedOption] = useState("null");
 	return (
 		<div className="feed-section-page">
@@ -38,8 +68,13 @@ function PostFeeds() {
 				</NavBarCard>
 			</div>
 			<div className="feed-section">
-				<PostCard />
+				{posts.map((post) => (
+					<div className="post-card" key={post.id}>
+						<PostCard props={post} />
+					</div>
+				))}
 			</div>
+			<div className="empty"></div>
 		</div>
 	);
 }
--- src/components/reusable/PostCard.js
+++ src/components/reusable/PostCard.js
@@ -3,18 +3,18 @@ import React from "react";
 import IconButton from "@mui/material/IconButton";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Icon } from "@iconify/react";
-function PostCard() {
+function PostCard({ props }) {
 	return (
 		<div className="card-post">
 			<div className="more-option-card">
 				<div className="profile-img-card">
-					<Avatar src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
+					<Avatar src={props.profileImage} />
 					<div className="profile-name-card">
 						<div className="name-span">
-							<h2>Noddyboi</h2>
-							<span>7 hours ago</span>
+							<h2>{props.userName}</h2>
+							<span>{props.createdAt}</span>
 						</div>
-						<p>Software Engineer</p>
+						<p>{props.profession}</p>
 					</div>
 				</div>
 				<IconButton aria-label="delete">
@@ -22,17 +22,11 @@ function PostCard() {
 				</IconButton>
 			</div>
 			<div className="post-title">
-				<h1>How to Become a data analyst by spending 0$</h1>
-				<p>
-					A lot of you want to know how much time is required to become Data
-					Analyst. Here is a tool-by-tool learning roadmap for freshers
-				</p>
+				<h1>{props.postTitle}</h1>
+				<p>{props.postComment}</p>
 			</div>
 			<div className="post-img-card">
-				<img
-					src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1506&q=80"
-					alt="Post"
-				/>
+				<img src={props.postImg} alt="Post" />
 			</div>
 			<div className="reaction-post-card">
 				<div className="icons-post-card">
@@ -51,7 +45,7 @@ function PostCard() {
 						className="comment"
 					/>
 				</div>
-				<p>5 comments</p>
+				<p>{props.comments.length} comments</p>
 			</div>
 		</div>
 	);
