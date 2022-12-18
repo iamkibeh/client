42727f2b14918f9c6adb348c9e8b4155f1dfd355
DenisOuma <dennisouma38@gmail.com>
2022-12-18 09:46:59 +0300

    Adding prop child navbar card that can be r used

--- src/components/pages/PostFeeds.jsx
+++ src/components/pages/PostFeeds.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import NavBarCard from "../reusable/NavBarCard";
 
 function PostFeeds() {
 	return (
 		<div>
-			<h1>Under maintainance</h1>
+			<NavBarCard className="cad-items">
+				<h1>Home</h1>
+				<div className="home-details"></div>
+			</NavBarCard>
 		</div>
 	);
 }
