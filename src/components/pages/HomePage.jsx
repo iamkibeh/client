30efacb4da78e5226d925e2b7470b5437b0c8588
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-01-09 22:59:07 +0300

    create and style hero section

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import HeroSection from '../moringa-clone-pages/HeroSection'
 
 const HomePage = () => {
-  return <div>Home page here</div>
+  return (
+    <div>
+      {/* Home page here */}
+      <HeroSection />
+    </div>
+  )
 }
 
 export default HomePage
